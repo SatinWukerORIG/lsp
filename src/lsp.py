41391d405e641a926a3f576010c144ef291b5e88
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-02-05 10:22:02 -0800

    Added colored text

--- src/lsp.py
+++ src/lsp.py
@@ -5,6 +5,7 @@ import os
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 from PIL import Image
+from termcolor import colored
 
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
@@ -26,7 +27,7 @@ def choose_img():
     name = random.choice(img_list)
     # Get the url of the chosen image
     url = f"https://raw.githubusercontent.com/SatinWuker/lsp/MASTER/lsp-db/{name}"
-    print(url)
+    print("访问 " + colored(url, 'green'))
 
     # Download the image
     with open(f"TempLSPimg.png", 'wb') as img_f:
@@ -39,7 +40,7 @@ def choose_img():
 def main():
     img_name = choose_img()
 
-    print(f"显示图片 [{img_name}]")
+    print(f"显示图片 [{colored(img_name, 'red')}]")
 
     # Show image
     image = Image.open(img_name)
