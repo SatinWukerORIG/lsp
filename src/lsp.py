8e43647b5bd9eace5ff5117d84dce0b9a2753a37
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-02-05 10:04:40 -0800

    Update lsp.py

--- src/lsp.py
+++ src/lsp.py
@@ -2,25 +2,38 @@ import requests
 import random
 import base64
 import os
-from os.path import exists
+from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 from PIL import Image
 
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+}
+
 
 def choose_img():
-    if exists("img-list.txt"):
-        with open("img-list.txt", encoding="utf-8") as img_list:
-            name = random.choice(img_list.readlines())
-            url = f"https://raw.githubusercontent.com/SatinWuker/lsp/MASTER/lsp-db/{name}.lsp"
 
-            with open(f"{name}.png", 'wb') as img_f:
-                lsp_content = requests.get(url).content
-                img_f.write(base64.b64decode(lsp_content))
+    # Read directories under lsp-db
+    re = requests.get("https://github.com/SatinWuker/lsp/tree/MASTER/lsp-db", headers=headers)
+    soup = BeautifulSoup(re.text, 'html.parser')
 
-                return f"{name}.png"
+    img_list = []      # all of the images names in the LSP database
 
-    else:
-        exit("找不到文件 [img-list.txt]")
+    for link in soup.find_all("a", class_="js-navigation-open Link--primary"):
+        img_list.append(link.get_text())
+
+    # Randomly choose one image in the img_list
+    name = random.choice(img_list)
+    # Get the url of the chosen image
+    url = f"https://raw.githubusercontent.com/SatinWuker/lsp/MASTER/lsp-db/{name}"
+    print(url)
+
+    # Download the image
+    with open(f"TempLSPimg.png", 'wb') as img_f:
+        lsp_content = requests.get(url).content
+        img_f.write(base64.b64decode(lsp_content))
+
+        return "TempLSPimg.png"
 
 
 def main():
@@ -28,10 +41,10 @@ def main():
 
     print(f"显示图片 [{img_name}]")
 
+    # Show image
     image = Image.open(img_name)
     image.show()
     os.remove(img_name)
-
 
 
 if __name__ == "__main__":
